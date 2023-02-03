187ad7189ffd04d1bab8969b817d36b6f0b17cc4
olivia <opierez@gmail.com>
2023-02-03 11:26:12 -0500

    commit tree 9a589e6d3eb5b70de7d67f376626e226c37748e2

---
+++ app/serializers/author_post_serializer.rb
@@ -0,0 +1,12 @@
+class AuthorPostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+  has_many :tags
+
+  def short_content
+    "#{self.object.content[0..39]}..." 
+    # byebug
+  end
+
+ 
+
+end
